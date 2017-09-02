18d2c8a291ed52d6939f208738fc372cd89d92b5
Will Ballard <wballard@mailframe.net>
2017-09-02 08:29:33 -0500

    simple convolutional model

--- train_mnist.py
+++ train_mnist.py
@@ -20,42 +20,20 @@ x_test = np.expand_dims(x_test / np.max(x_test), -1)
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
-# our deep model
+# our convolutional model
 input_shape = x_train[0].shape
-classes = 10
-inputs = Input(shape=input_shape)
-# Block 1
-x = Conv2D(64, (3, 3), activation='relu',
-           padding='same', name='block1_conv1')(inputs)
-x = Conv2D(64, (3, 3), activation='relu',
-           padding='same', name='block1_conv2')(x)
-x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
-
-# Block 2
-x = Conv2D(128, (3, 3), activation='relu',
-           padding='same', name='block2_conv1')(x)
-x = Conv2D(128, (3, 3), activation='relu',
-           padding='same', name='block2_conv2')(x)
-x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
-
-# Block 3
-x = Conv2D(256, (3, 3), activation='relu',
-           padding='same', name='block3_conv1')(x)
-x = Conv2D(256, (3, 3), activation='relu',
-           padding='same', name='block3_conv2')(x)
-x = Conv2D(256, (3, 3), activation='relu',
-           padding='same', name='block3_conv3')(x)
-x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
-
-# Classification block
-x = Flatten(name='flatten')(x)
-x = Dense(512, activation='relu', name='fc1')(x)
-x - BatchNormalization()(x)
-x = Dense(512, activation='relu', name='fc2')(x)
-x - BatchNormalization()(x)
-x = Dense(classes, activation='softmax', name='predictions')(x)
-
-model = Model(inputs, x)
+num_classes = 10
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 print(model.summary())
 
 model.compile(loss='categorical_crossentropy',
